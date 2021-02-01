5867fa1b33723a23e114829719474de16ddca06e
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-01 13:48:27 -0300

    else if

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -9,9 +9,12 @@ public class Main {
     public static void main(String[] args) {
         boolean ePar = (11 % 2)  == 0;
 
-        if (ePar)
-            System.out.println("O número é par!");
-        else
-            System.out.println("O número é impar");
+        if (10 % 2 == 1) {
+            System.out.println("bugou");
+        } else if (10 % 2 == 0) {
+            System.out.println("o número é par");
+        } else {
+            System.out.println("sei la");
+        }
     }
 }
