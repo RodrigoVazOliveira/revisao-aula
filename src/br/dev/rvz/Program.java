0163f8e27be2c13fdbc6876f2fc55ddefd45b711
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-01 16:13:34 -0300

    novo aplicativo

---
+++ src/br/dev/rvz/Program.java
@@ -0,0 +1,15 @@
+package br.dev.rvz;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        boolean continuar = true;
+        Scanner in = new Scanner(System.in);
+        while (continuar) {
+            System.out.println("Qual zoologico vai cadastrar?");
+            String zoo = in.nextLine();
+
+        }
+    }
+}
