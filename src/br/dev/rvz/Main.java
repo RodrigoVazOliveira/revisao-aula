46d69f2763cae39f721cd42aeace33358383da6b
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-01 14:10:22 -0300

    do while, while

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -1,20 +1,30 @@
 package br.dev.rvz;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
-        boolean ePar = (11 % 2)  == 0;
-
-        if (10 % 2 == 1) {
-            System.out.println("bugou");
-        } else if (10 % 2 == 0) {
-            System.out.println("o número é par");
-        } else {
-            System.out.println("sei la");
+        /*
+        * Estrutura de repetição
+        *
+        * */
+        boolean continuar = true;
+        int number = 0;
+        while (continuar) {
+            System.out.println(number);
+            number++;
+            if (number == 10)
+                continuar = false;
         }
+
+        number = 0;
+        continuar = true;
+        System.out.println("");
+        do {
+            System.out.println(number);
+            number++;
+            if (number == 10)
+                continuar = false;
+        } while (continuar);
+
     }
 }
