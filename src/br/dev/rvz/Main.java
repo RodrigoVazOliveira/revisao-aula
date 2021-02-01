d003886cb39a0401bafe21eeccbb59afe411697d
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-01 14:24:37 -0300

    For e foreach com lista de números

--- src/br/dev/rvz/Main.java
+++ src/br/dev/rvz/Main.java
@@ -1,5 +1,8 @@
 package br.dev.rvz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -25,6 +28,32 @@ public class Main {
             if (number == 10)
                 continuar = false;
         } while (continuar);
+
+
+        /*
+        *  usando for e foreach
+        * */
+
+        for (int i = 0; i < 13; i++) {
+            System.out.println("O número no for é: " + i);
+        }
+
+         /*
+         *  for eeach
+         *
+         * */
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            numbers.add(i);
+        }
+
+        /*
+        *  Listando com for each
+        * */
+
+        for (Integer n : numbers) {
+            System.out.println("lista de números: " + n);
+        }
 
     }
 }
